eedada08f8de6ee0b364e6d580ee4e7afcb11eb3
MolinaMendozaCarlosFabricio <233376@ids.upchiapas.edu.mx>
2025-01-11 13:10:09 -0600

    Agregando pantalla de carga a la API

--- src/components/loaders/api-loader/ApiLoader.tsx
+++ src/components/loaders/api-loader/ApiLoader.tsx
@@ -1,7 +1,13 @@
 import styles from './ApiLoader.module.scss';
+import './ApiLoader.css'
+import LoadingSpinner from '../../../img/Blocks@1x-1.0s-200px-200px.svg';
 
 function ApiLoader() {
-  return <p className={styles.loader}>Initializing API</p>;
+  return (
+    <img src={LoadingSpinner} className='img_loader_spinner'></img>
+  );
 }
+
+// <p className={styles.loader}>Initializing API</p>
 
 export { ApiLoader };
